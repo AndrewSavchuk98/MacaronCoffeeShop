428f9bf27fb3b89f27d97960ce120d86abca775c
andrej.savchuk.110 <andrej.petrenko.110@gmail.com>
2023-06-11 11:57:17 +0300

    base gift fragment

--- app/src/main/java/com/savchukandrew/macaroncoffeeshop/features/gift/presentation/GiftFragment.kt
+++ app/src/main/java/com/savchukandrew/macaroncoffeeshop/features/gift/presentation/GiftFragment.kt
@@ -1,9 +1,9 @@
-package com.savchukandrew.macaroncoffeeshop.features.location.presentation
+package com.savchukandrew.macaroncoffeeshop.features.gift.presentation
 
 import androidx.fragment.app.Fragment
 import com.savchukandrew.macaroncoffeeshop.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LocationFragment: Fragment(R.layout.fragment_location) {
+class GiftFragment : Fragment(R.layout.fragment_gift) {
 }
