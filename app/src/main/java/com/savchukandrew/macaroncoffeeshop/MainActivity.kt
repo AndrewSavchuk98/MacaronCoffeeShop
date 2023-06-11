73284c9acc5cb0bd8b0bd98db7ba2ef5bd201568
andrej.savchuk.110 <andrej.petrenko.110@gmail.com>
2023-06-11 09:46:55 +0300

    Added bottom Nav view

--- app/src/main/java/com/savchukandrew/macaroncoffeeshop/MainActivity.kt
+++ app/src/main/java/com/savchukandrew/macaroncoffeeshop/MainActivity.kt
@@ -2,12 +2,26 @@ package com.savchukandrew.macaroncoffeeshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.savchukandrew.macaroncoffeeshop.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
+
     }
 }
