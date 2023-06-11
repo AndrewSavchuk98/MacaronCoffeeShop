10b4d1f7d2e0a89debc64865f0dd708104f7b146
andrej.savchuk.110 <andrej.petrenko.110@gmail.com>
2023-06-11 11:35:35 +0300

    added location screen

---
+++ app/src/main/java/com/savchukandrew/macaroncoffeeshop/features/location/presentation/LocationFragment.kt
@@ -0,0 +1,7 @@
+package com.savchukandrew.macaroncoffeeshop.features.location.presentation
+
+import androidx.fragment.app.Fragment
+import com.savchukandrew.macaroncoffeeshop.R
+
+class LocationFragment: Fragment(R.layout.fragment_location) {
+}
